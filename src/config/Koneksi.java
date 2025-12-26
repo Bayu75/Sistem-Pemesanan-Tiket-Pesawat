e091da09dccde75f66d8f3e8b3b0ea9a0b55cf17
unknown <kadekbayu322@gmail.com>
2025-12-26 19:08:25 +0800

    First Commit

---
+++ src/config/Koneksi.java
@@ -0,0 +1,30 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Koneksi {
+    
+    public static void main(String[] args) {
+    getKoneksi();
+    }
+    
+    private static Connection koneksi;
+
+    public static Connection getKoneksi() {
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/tiket_pesawat";
+                String user = "root";
+                String pass = "";
+
+                koneksi = DriverManager.getConnection(url, user, pass);
+                System.out.println("Koneksi berhasil");
+            } catch (SQLException e) {
+                System.out.println("Koneksi gagal: " + e.getMessage());
+            }
+        }
+        return koneksi;
+    }
+}
